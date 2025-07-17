02cd48a251c478beb0b40dfc1c806f0ab418654b
Vankipuram Chandu <vankipuram.chandu@perficient.com>
2025-07-17 16:17:40 +0530

    Initial commit - Kafka JSON batch producer

---
+++ src/main/java/com/poc/JsonFor/JsonForApplication.java
@@ -0,0 +1,23 @@
+package com.poc.JsonFor;
+
+import com.poc.JsonFor.Service.BatchInsertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonForApplication implements CommandLineRunner {
+
+	@Autowired
+	private BatchInsertService service;
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonForApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		service.publishCustomersFromJson("src/main/resources/customers.json");
+	}
+}
